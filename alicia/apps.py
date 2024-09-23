f30257c8b2a16394842be15926126e5f0b20a330
katasrofi <danielsensei42@gmail.com>
2024-09-23 16:34:35 +0700

    Initial Commit

--- alicia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AliciaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alicia'
--- alicia/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def HomeScreenView(request):
-    return render(request, 'base.html', {})
